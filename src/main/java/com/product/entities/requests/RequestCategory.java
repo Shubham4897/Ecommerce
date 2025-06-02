ee8f2b7108c0fac1d90d6834447459bc82842af4
shubham <shubhamsingh4897@gmail.com>
2025-06-02 13:43:57 +0530

    Commited Project Files

---
+++ src/main/java/com/product/entities/requests/RequestCategory.java
@@ -0,0 +1,16 @@
+package com.product.entities.requests;
+
+public class RequestCategory {
+	
+	private String categoryName;
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+
+}
